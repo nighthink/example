97e316b1b35c9a4d0923a33c46d0f178fff202a3
Nighthink <1660553137@qq.com>
2019-12-18 22:03:09 +0800

    Add WelcomeInMessageDialogBox.java

---
+++ java/WelcomeInMessageDialogBox.java
@@ -0,0 +1,9 @@
+//This application program displays Welcome to Java! in a message dialog box.
+
+import javax.swing.JOptionPane;
+
+public class WelcomeInMessageDialogBox{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    }
+}
