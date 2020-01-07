5d267414bdfee4d3e2d4662bdc04ceefae1c8bbd
Nighthink <1660553137@qq.com>
2020-01-07 21:01:57 +0800

    add Android

---
+++ Android/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        /*Intent intent2 = getIntent();
+        String data = intent2.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);*/
+       Button button2 = (Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+}
